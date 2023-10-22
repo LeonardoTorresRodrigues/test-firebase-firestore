30d45adba0cb85d0d63ec2b665ea65db5836866a
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-10-22 13:59:29 -0300

    aula 5: Observer onSnapshot

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { addPlansAcess, setPlansAcess } from './services/dataAcess/plansAcess';
 import { getPlansAction, setPlansAction, updatePlansAction } from './services/actions/plansAction';
+import { getPlansObserver } from './services/observers/plansObservers';
 
 function App() {
+  const [plans, setPlans] = useState<any[]>([]);
   useEffect(() => {
-    getPlansAction().then((res) => {
-      console.log(res);
-    });
+    getPlansObserver(setPlans)
   }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {plans.map((elem) => (
+        <p>{elem.text}</p>
+      ))}
     </div>
   );
 }
